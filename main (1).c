d2d60d53a5aeda45d086a31da7685dc8753e0d24
Marcela Souza <116609831+parkboomm@users.noreply.github.com>
2023-03-30 00:16:36 -0300

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,89 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void soma()
+{ 
+    float valor1, valor2, result; 
+    printf("\nDigite o 1 valor: "); 
+    scanf ("%f", &valor1); 
+    printf("Digite o 2 valor: "); 
+    scanf ("%f", &valor2); 
+    result = valor1 + valor2; 
+    printf("\nResultado: %.2f\n\n ", result); 
+}
+void subtrai(){ 
+    float valor1, valor2, result; 
+    printf("\nDigite o 1 valor: "); 
+    scanf ("%f", &valor1); 
+    printf("Digite o 2 valor: "); 
+    scanf ("%f", &valor2);
+    result = valor1 - valor2; 
+    printf("\nResultado: %.2f\n\n ", result); 
+}
+void divide(){ 
+    float valor1, valor2, result;
+    printf("\nDigite o 1 valor: ");
+    scanf ("%f", &valor1);
+    printf("Digite o 2 valor: ");
+    scanf ("%f", &valor2);
+    result = valor1 / valor2;
+    printf("\nResultado: %.2f\n\n ", result);
+    if(valor2 != 0)
+            printf("%.2f\n\n", valor1 / valor2);
+            else
+                printf("Nao existe divisao por 0\n\n");
+}
+void multiplica()
+{
+    float valor1, valor2, result;
+    printf("\nDigite o 1 valor: ");
+    scanf ("%f", &valor1);
+    printf("Digite o 2 valor: ");
+    scanf ("%f", &valor2);
+    result = valor1 * valor2;
+    printf("\nResultado: %.2f\n\n ", result);
+}
+void menu(){ 
+    int escolha; 
+    printf("Selecione uma operacao matematica:\n"); // msg na tela
+    printf("\t1- Adicao\n");
+    printf("\t2- Subtracao\n");
+    printf("\t3- Divisao\n");
+    printf("\t4- Multiplicacao\n");
+    printf("Operacao: "); 
+    scanf ("%d", &escolha); 
+
+    switch (escolha){ 
+        case 1: 
+            soma(); 
+        break; 
+
+        case 2:
+            subtrai();
+        break;
+
+        case 3:
+            divide();
+            
+        break;
+
+        case 4:
+            multiplica();
+            break;
+
+
+    }
+}
+
+
+int main()
+{
+    menu(); 
+    return 0; 
+}
